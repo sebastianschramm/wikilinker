41a6503736a6234084b58dcd322cf5d5adcbf301
Sebastian Schramm <schrammsm@gmail.com>
2023-09-12 19:24:56 +0200

    add wikilinker module

---
+++ wikilinker/ner.py
@@ -0,0 +1,42 @@
+import json
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+
+def get_ner_generator(model_name: str) -> pipeline:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+
+    ner_gen = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        do_sample=False,
+        max_new_tokens=100,
+        return_full_text=False,
+    )
+    return ner_gen
+
+
+def get_prompt_template():
+    pt = """A virtual assistant answers questions from
+         a user based on the provided text.
+         USER: Text: {input_text}
+         ASSISTANT: I’ve read this text.
+         USER: What describes {entity_name} in the text?
+         ASSISTANT:
+         """
+    return pt
+
+
+def get_ner(input_text: str, entity_type: str, model_name: str):
+    prompt = get_prompt_template().format_map(
+        {"input_text": input_text, "entity_name": entity_type.lower()}
+    )
+    try:
+        ner_list = json.loads(
+            get_ner_generator(model_name=model_name)(prompt)[0]["generated_text"]
+        )
+    except Exception:
+        ner_list = []
+    return ner_list
